bdace4acaa31b36a70b4fa4081801cc385b277d4
xiaosong <kunkkaco@gmail.com>
2019-07-31 17:56:49 +0800

    fix: 删除无用的测试文件

--- packages/login/config-overrides.js
+++ packages/login/config-overrides.js
@@ -9,16 +9,16 @@ const paths = require('react-scripts/config/paths');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
-paths.appIndexJs = `${paths.appSrc}/pages/index.js`;
+// paths.appIndexJs = `${paths.appSrc}/pages/index.js`;
 // paths.appLoginJs = `${paths.appSrc}/pages/login.js`;
-paths.servedPath = './';
+// paths.servedPath = './';
 
-const getEntryConfig = env => {
-  const arr = 'development' === env ? [require.resolve('react-dev-utils/webpackHotDevClient')] : [];
-  return entry => {
-    return [...arr, `${paths.appSrc}/pages/${entry}.js`];
-  };
-};
+// const getEntryConfig = env => {
+//   const arr = 'development' === env ? [require.resolve('react-dev-utils/webpackHotDevClient')] : [];
+//   return entry => {
+//     return [...arr, `${paths.appSrc}/pages/${entry}.js`];
+//   };
+// };
 
 const removePlugin = (plugins, name) => {
   const list = plugins.filter(it => !(it.constructor && it.constructor.name && name === it.constructor.name));
@@ -28,71 +28,73 @@ const removePlugin = (plugins, name) => {
   return list;
 };
 
-const genHtmlWebpackPlugin = env => {
-  const minify = {
-    removeComments: true,
-    collapseWhitespace: true,
-    removeRedundantAttributes: true,
-    useShortDoctype: true,
-    removeEmptyAttributes: true,
-    removeStyleLinkTypeAttributes: true,
-    keepClosingSlash: true,
-    minifyJS: {
-      comments: '@cc_on',
-    },
-    minifyCSS: true,
-    minifyURLs: true,
-  };
-  const config = Object.assign(
-    {},
-    { inject: true, template: paths.appHtml },
-    'development' !== env ? { minify } : undefined,
-  );
-  return entry => {
-    return new HtmlWebpackPlugin({
-      ...config,
-      chunks: ['vendors', `runtime~${entry}.html`, entry],
-      filename: `${entry}.html`,
-    });
-  };
-};
+// const genHtmlWebpackPlugin = env => {
+//   const minify = {
+//     removeComments: true,
+//     collapseWhitespace: true,
+//     removeRedundantAttributes: true,
+//     useShortDoctype: true,
+//     removeEmptyAttributes: true,
+//     removeStyleLinkTypeAttributes: true,
+//     keepClosingSlash: true,
+//     minifyJS: {
+//       comments: '@cc_on',
+//     },
+//     minifyCSS: true,
+//     minifyURLs: true,
+//   };
+//   const config = Object.assign(
+//     {},
+//     { inject: true, template: paths.appHtml },
+//     'development' !== env ? { minify } : undefined,
+//   );
+//   return entry => {
+//     return new HtmlWebpackPlugin({
+//       ...config,
+//       chunks: ['vendors', `runtime~${entry}.html`, entry],
+//       filename: `${entry}.html`,
+//     });
+//   };
+// };
 
-const supportMultiPage = (config, env) => {
-  const list = ['index', 'login'];
-  config.entry = {};
-  config.plugins = removePlugin(config.plugins, 'HtmlWebpackPlugin');
-  const getEntry = getEntryConfig(env);
-  const getHtmlWebpackPlugin = genHtmlWebpackPlugin(env);
-  list.forEach(it => {
-    config.entry[it] = getEntry(it);
-    config.plugins.push(getHtmlWebpackPlugin(it));
-  });
-
-  if ('development' === env) {
-    config.output.filename = 'static/js/[name].bundle.js';
-  } else {
-    config.plugins = removePlugin(config.plugins, 'GenerateSW');
-    const workboxWebpackPlugin = new WorkboxWebpackPlugin.GenerateSW({
-      clientsClaim: true,
-      exclude: [/\.map$/, /asset-manifest\.json$/, /\.html$/],
-      importWorkboxFrom: 'local',
-      // navigateFallback: paths.servedPath + '/index.html',
-      // navigateFallbackBlacklist: [
-      //   // Exclude URLs starting with /_, as they're likely an API call
-      //   new RegExp('^/_'),
-      //   // Exclude URLs containing a dot, as they're likely a resource in
-      //   // public/ and not a SPA route
-      //   new RegExp('/[^/]+\\.[^/]+$'),
-      // ],
-    });
-    config.plugins.push(workboxWebpackPlugin);
+const overrideGenerateSWConfig = (config, env) => {
+  if ('development' !== env) {
+    return config;
   }
+  config.plugins = removePlugin(config.plugins, 'GenerateSW');
+  const workboxWebpackPlugin = new WorkboxWebpackPlugin.GenerateSW({
+    clientsClaim: true,
+    exclude: [/\.map$/, /asset-manifest\.json$/],
+    importWorkboxFrom: 'local',
+    navigateFallback: publicUrl + '/index.html',
+    navigateFallbackBlacklist: [
+      // Exclude URLs starting with /_, as they're likely an API call
+      new RegExp('^/_'),
+      // Exclude URLs containing a dot, as they're likely a resource in
+      // public/ and not a SPA route
+      new RegExp('/[^/]+\\.[^/]+$'),
+    ],
+  });
+  // const workboxWebpackPlugin = new WorkboxWebpackPlugin.GenerateSW({
+  //   clientsClaim: true,
+  //   exclude: [/\.map$/, /asset-manifest\.json$/, /\.html$/],
+  //   importWorkboxFrom: 'local',
+  //   navigateFallback: paths.servedPath + '/index.html',
+  //   navigateFallbackBlacklist: [
+  //     // Exclude URLs starting with /_, as they're likely an API call
+  //     new RegExp('^/_'),
+  //     // Exclude URLs containing a dot, as they're likely a resource in
+  //     // public/ and not a SPA route
+  //     new RegExp('/[^/]+\\.[^/]+$'),
+  //   ],
+  // });
+  config.plugins.push(workboxWebpackPlugin);
   return config;
 };
 
 module.exports = {
   webpack: override(
-    supportMultiPage,
+    overrideGenerateSWConfig,
     fixBabelImports('import', {
       libraryName: 'antd',
       libraryDirectory: 'es',
@@ -111,11 +113,11 @@ module.exports = {
     }),
     // addBundleVisualizer(),
   ),
-  devServer: configFunction => {
-    return (proxy, allowedHost) => {
-      const config = configFunction(proxy, allowedHost);
-      config.historyApiFallback.rewrites = [{ from: /^\/login\.html/, to: '/build/login.html' }];
-      return config;
-    };
-  },
+  // devServer: configFunction => {
+  //   return (proxy, allowedHost) => {
+  //     const config = configFunction(proxy, allowedHost);
+  //     config.historyApiFallback.rewrites = [{ from: /^\/login\.html/, to: '/build/login.html' }];
+  //     return config;
+  //   };
+  // },
 };
