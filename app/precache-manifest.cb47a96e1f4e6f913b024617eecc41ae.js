63fff7599b1336459d1df3e7aa50ba8b557e4916
xiaosong <kunkkaco@gmail.com>
2019-08-05 11:08:31 +0800

    Updates

---
+++ app/precache-manifest.cb47a96e1f4e6f913b024617eecc41ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd721e7464619e70da8161d03b82b13",
+    "url": "/cra-lerna-template/app/index.html"
+  },
+  {
+    "revision": "36ac62803645963710c5",
+    "url": "/cra-lerna-template/app/static/css/2.e4a195b8.chunk.css"
+  },
+  {
+    "revision": "d98bdc73b62b1757e92b",
+    "url": "/cra-lerna-template/app/static/css/main.37eff0ce.chunk.css"
+  },
+  {
+    "revision": "36ac62803645963710c5",
+    "url": "/cra-lerna-template/app/static/js/2.558816d2.chunk.js"
+  },
+  {
+    "revision": "d98bdc73b62b1757e92b",
+    "url": "/cra-lerna-template/app/static/js/main.cf87c7c9.chunk.js"
+  },
+  {
+    "revision": "0c502171bb18110cd40f",
+    "url": "/cra-lerna-template/app/static/js/runtime~main.c6560fbe.js"
+  }
+]);
