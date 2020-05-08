51319610633cb0db718926b313088a21cddd4b2d
xiaosong <kunkkaco@gmail.com>
2020-05-08 12:51:13 +0800

    Updates

---
+++ login/precache-manifest.df72ff5288f9e6b5c06dae8aefb9e5e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d142895e7ffe629374f44aa29644a8a6",
+    "url": "/cra-lerna-template/login/index.html"
+  },
+  {
+    "revision": "8033de86434f2dbfbb9f",
+    "url": "/cra-lerna-template/login/static/css/2.f756f010.chunk.css"
+  },
+  {
+    "revision": "29c6063ef1bcd33ec70b",
+    "url": "/cra-lerna-template/login/static/css/main.b29915ec.chunk.css"
+  },
+  {
+    "revision": "8033de86434f2dbfbb9f",
+    "url": "/cra-lerna-template/login/static/js/2.8d2a1ba7.chunk.js"
+  },
+  {
+    "revision": "db6251776d83a9b97a8ae2f1e6a03ac3",
+    "url": "/cra-lerna-template/login/static/js/2.8d2a1ba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29c6063ef1bcd33ec70b",
+    "url": "/cra-lerna-template/login/static/js/main.498242ee.chunk.js"
+  },
+  {
+    "revision": "19ead4d28ae8124ea5da",
+    "url": "/cra-lerna-template/login/static/js/runtime-main.2043737c.js"
+  }
+]);
